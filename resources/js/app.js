41b5ce84e338ed720aa852b428cb83a387f5fcb5
fightgood <f_allmyheart@hotmail.com>
2022-10-23 18:54:48 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+// app.js
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./App.vue";
+createApp(App).mount("#app");
---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>How To Install Vue 3 in Laravel 9 with Vite</title>
+
+        @vite('resources/css/app.css')
+    </head>
+
+    <body>
+        <div id="app"></div>
+
+        @vite('resources/js/app.js')
+    </body>
+
+</html>
